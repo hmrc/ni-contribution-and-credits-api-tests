a2b21bab162dd0ba3f970ba65835e4a79ced7347
swapnatootikoora1 <170416052+swapnatootikoora@users.noreply.github.com>
2024-05-22 15:13:45 +0100

    updated the basespec.scala file

--- src/test/scala/uk/gov/hmrc/api/specs/BaseSpec.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/BaseSpec.scala
@@ -19,5 +19,52 @@ package uk.gov.hmrc.api.specs
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.GivenWhenThen
+import io.findify.s3mock.S3Mock
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import uk.gov.hmrc.test.api.service.BankAccountReputationService
 
-trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers {}
+import java.io.File
+
+//trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers {
+  trait BaseSpec extends AnyWordSpec with BeforeAndAfterEach with Matchers with BeforeAndAfterAll {
+
+    val config: Config = ConfigFactory.load()
+    val service        = new BankAccountReputationService
+
+    val SUREPAY_PATH     = "/surepay/v1/gateway"
+    val defaultUserAgent = "bars-acceptance-tests"
+
+    private val random     = new java.util.Random()
+    private val s3MockPort = config.getInt("mock.s3.port")
+
+    val s3Dir          = new File(getClass.getResource("/sThreeBucket").getFile())
+    private val s3Mock = S3Mock(port = s3MockPort, dir = s3Dir.getAbsolutePath)
+
+    override def beforeAll(): Unit = {
+      s3Mock.start
+      service.postRefreshEiscdCache()
+      service.postRefreshModcheckCache()
+    }
+
+    override def afterAll(): Unit =
+      s3Mock.shutdown
+
+    def randomAlphaChar(): Char = {
+      val low  = 97
+      val high = 122
+      (random.nextInt(high - low) + low).toChar
+    }
+
+    def randomString(length: Int = 5): String =
+      (0 until length).map(_ => randomAlphaChar()).mkString
+
+    def generateRandomName: Option[String] =
+      Some(s"Mr Na${randomString()} ${randomString()} ${randomString(8)}-Smith")
+
+    def generateRandomBusinessName: Option[String] =
+      Some(s"O'C${randomString()} ${randomString(8)} ${randomString(4)} LLC")
+  }
+
+}//
