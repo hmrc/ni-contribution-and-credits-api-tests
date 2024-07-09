cadb7ed2a49d29780e1e70bb8aac4b15dc00caaa
swapnatootikoora1 <170416052+swapnatootikoora1@users.noreply.github.com>
2024-07-09 09:25:04 +0100

    Started auth token implementation

--- src/test/scala/uk/gov/hmrc/api/service/NiccService.scala
+++ src/test/scala/uk/gov/hmrc/api/service/NiccService.scala
@@ -28,7 +28,7 @@ import scala.concurrent.duration._
 class NiccService extends HttpClient {
 
   val host: String                   = TestConfiguration.url("nicc")
-  val token = generateToken("nicc")
+  //val token = generateToken("nicc")
 
   def makeRequest(request: Request, startTaxYear: String, endTaxYear: String): StandaloneWSRequest#Self#Response = {
 
@@ -39,7 +39,7 @@ class NiccService extends HttpClient {
       post(
         url,
         Json.stringify(requestPayload),
-        ("Authorization", token),
+       // ("Authorization", token),
         ("Content-Type", "application/json")
       ),
       10.seconds
