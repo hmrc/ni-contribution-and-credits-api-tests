7c703bca09f7c16fa16d4330c1064438f23427c9
swapnatootikoora1 <170416052+swapnatootikoora1@users.noreply.github.com>
2024-09-17 18:11:30 +0100

    year validated

--- src/test/scala/uk/gov/hmrc/api/helpers/BaseHelper.scala
+++ src/test/scala/uk/gov/hmrc/api/helpers/BaseHelper.scala
@@ -5,17 +5,23 @@ import java.time.format.{DateTimeFormatter, DateTimeParseException}
 trait BaseHelper {
   def ValidateDOB(dateString: String): Boolean = {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-      try
-        {
-val date = LocalDate.parse(dateString,formatter)
-          val today = LocalDate.now()
-          val difference = Period.between(date,today)
-          difference.getYears>16
-        } catch{
-        case e: DateTimeParseException =>
-          println("error parsing date", e.getMessage)
-          false
-      }
+    try {
+      val date = LocalDate.parse(dateString, formatter)
+      val today = LocalDate.now()
+      val difference = Period.between(date, today)
+      difference.getYears > 16
+    } catch {
+      case e: DateTimeParseException =>
+        println("error parsing date", e.getMessage)
+        false
+    }
   }
 
+  def ValidateStartTaxYear(yearString: String, minYear: Int = 1975, maxYear: Int = 2023): Boolean = {
+
+yearString.matches("""\d{4}""") && {
+  val year = yearString.toInt
+  year >= minYear && year  <= maxYear
+}
+  }
 }
