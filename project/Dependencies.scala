152931287a368e5e8addae7095497b975ab3589f
matjoseph24 <mathew.a.joseph@accenture.com>
2025-04-15 10:13:22 +0100

    DL-15961: Addressed pr bot comment

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,7 @@ object Dependencies {
     "com.typesafe"         % "config"                  % "1.4.3"   % Test,
     "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.2.11"  % Test,
     "com.typesafe.play"   %% "play-ws-standalone-json" % "2.2.11"  % Test,
+    "uk.gov.hmrc"         %% "api-test-runner"         % "0.9.0"   % Test,
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"  % Test,
     "org.scalatest"       %% "scalatest"               % "3.2.19"  % Test,
     "org.slf4j"            % "slf4j-simple"            % "2.0.17"  % Test,
