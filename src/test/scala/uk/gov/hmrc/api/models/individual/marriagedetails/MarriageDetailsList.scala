91c30dc4113f43ad1dccf45090583615b47a3dce
vedhashree-hub <247391969+vedhashree-hub@users.noreply.github.com>
2026-01-13 10:15:29 +0000

    DWPDAT-49: Created models for Individual Marriage details

---
+++ src/test/scala/uk/gov/hmrc/api/models/individual/marriagedetails/MarriageDetailsList.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.models.individual.marriagedetails
+
+import play.api.libs.json.{Json, OFormat}
+
+import java.time.LocalDate
+
+final case class MarriageDetailsList(
+    sequenceNumber: Int,
+    status: String,
+    startDate: Option[LocalDate] = None,
+    startDateStatus: Option[String] = None,
+    endDate: Option[LocalDate] = None,
+    endDateStatus: Option[String] = None,
+    spouseIdentifier: Option[String] = None,
+    spouseForename: Option[String] = None,
+    spouseSurname: Option[String] = None,
+    separationDate: Option[LocalDate] = None,
+    reconciliationDate: Option[LocalDate] = None
+)
+
+object MarriageDetailsList {
+  implicit val format: OFormat[MarriageDetailsList] = Json.format[MarriageDetailsList]
+}
