ee81bdc49bd92b54bd9481993f12218cbd51f758
Vedha <247391969+vedhashree-hub@users.noreply.github.com>
2026-01-21 14:02:46 +0000

    DWPDAT-52: Created models for liability summary details api (#29)

---
+++ src/test/scala/uk/gov/hmrc/api/models/liabilitySummary/LiabilityDetails.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.models.liabilitySummary
+
+import play.api.libs.json.{Json, OFormat}
+
+final case class LiabilityDetails(
+    identifier: String,
+    `type`: String,
+    occurrenceNumber: Int,
+    startDate: String,
+    startDateStatus: Option[String],
+    endDateStatus: Option[String],
+    endDate: Option[String],
+    country: Option[String],
+    trainingCreditApprovalStatus: Option[String],
+    casepaperReferenceNumber: Option[String],
+    homeResponsibilitiesProtectionBenefitReference: Option[String],
+    homeResponsibilitiesProtectionRate: Option[BigDecimal],
+    homeResponsibilitiesProtectionIndicator: Option[String],
+    lostCardNotificationReason: Option[String],
+    lostCardRulingReason: Option[String],
+    homeResponsibilityProtectionCalculationYear: Option[Int],
+    awardAmount: Option[BigDecimal],
+    resourceGroupIdentifier: Option[Int],
+    officeDetails: Option[OfficeDetails]
+)
+
+object LiabilityDetails {
+
+  implicit val format: OFormat[LiabilityDetails] =
+    Json.format[LiabilityDetails]
+
+}
