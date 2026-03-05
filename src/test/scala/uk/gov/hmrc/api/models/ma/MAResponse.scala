932b4472f21821ccd4059f7fdd34e84abaf74144
OltionF <83361413+OltionF@users.noreply.github.com>
2026-03-05 10:05:21 +0000

    DWPDAT-101 Developed test scenarios for MA benefit line

---
+++ src/test/scala/uk/gov/hmrc/api/models/ma/MAResponse.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.models.ma
+
+import play.api.libs.json.{Format, Json, OFormat, Writes}
+import uk.gov.hmrc.api.models.esajsa.NIContributionsAndCreditsResult
+
+import java.time.LocalDate
+
+case class FilteredClass2MaReceipts(
+    receiptDates: List[LocalDate]
+)
+
+object FilteredClass2MaReceipts {
+  implicit val filteredClass2MaReceiptsWrites: Format[FilteredClass2MaReceipts] = Json.format[FilteredClass2MaReceipts]
+}
+
+case class FilteredLiabilitySummaryDetailItem(
+    startDate: LocalDate,
+    endDate: Option[LocalDate]
+)
+
+object FilteredLiabilitySummaryDetailItem {
+
+  implicit val filteredLiabilitySummaryDetailItemWrites: Format[FilteredLiabilitySummaryDetailItem] =
+    Json.format[FilteredLiabilitySummaryDetailItem]
+
+}
+
+case class FilteredLiabilitySummaryDetails(
+    liabilityDetails: List[FilteredLiabilitySummaryDetailItem]
+)
+
+object FilteredLiabilitySummaryDetails {
+
+  implicit val filteredLiabilitySummaryDetailsWrites: Format[FilteredLiabilitySummaryDetails] =
+    Json.format[FilteredLiabilitySummaryDetails]
+
+}
+
+case class MAResponse(
+    benefitType: String,
+    nationalInsuranceNumber: String,
+    class2MAReceiptsResult: FilteredClass2MaReceipts,
+    liabilitySummaryDetailsResult: List[FilteredLiabilitySummaryDetails],
+    niContributionsAndCreditsResult: NIContributionsAndCreditsResult
+)
+
+object MAResponse {
+  implicit val format: Format[MAResponse] = Json.format[MAResponse]
+}
---
+++ src/test/scala/uk/gov/hmrc/api/specs/ma_specs/MAScenarios.scala
@@ -0,0 +1,137 @@
+package uk.gov.hmrc.api.specs.ma_specs
+
+import org.scalatest.{BeforeAndAfterAll, Ignore}
+import play.api.libs.json.Json
+import play.api.libs.ws.StandaloneWSRequest
+import uk.gov.hmrc.api.helpers.BaseHelper
+import uk.gov.hmrc.api.models.ma.{MARequest, MAResponse}
+import uk.gov.hmrc.api.service.MAService
+import uk.gov.hmrc.api.specs.BaseSpec
+import uk.gov.hmrc.api.utils.JsonUtils
+
+@ignore
+class MAScenarios extends BaseSpec with BaseHelper with BeforeAndAfterAll {
+
+  val maService                              = new MAService
+  var PayloadMapping: Map[String, MARequest] = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    val jsonString = JsonUtils.readJsonFile(
+      "src/test/scala/uk/gov/hmrc/api/testData/MA_TestData.json"
+    )
+    println(jsonString)
+    PayloadMapping = JsonUtils.parseJsonToMARequestMap(jsonString) match {
+      case Left(failure) => fail(s"Parsing failed: $failure")
+      case Right(map)    => map
+    }
+  }
+
+  Feature(s"Test Scenarios for MA Benefit Type") {
+
+    Scenario(s"MA_PTC001: Verify full MA response body for a valid NINO with Suffix") {
+
+      Given("The Benefit eligibility Info API is up and running for MA")
+
+      When("A request for MA is sent")
+
+      val payloadKey = "MA_PTC001"
+      val payload    = PayloadMapping.getOrElse(payloadKey, fail(s"$payloadKey not found"))
+
+      val response = maService.makeRequest(payload, payloadKey)
+
+      val result: MAResponse = assertMAResponse(payload, response)
+
+      println(s"Response Status: ${response.status} ${response.statusText}")
+      println(Json.prettyPrint(Json.toJson(result)))
+    }
+
+    Scenario(s"MA_PTC002: Verify full MA response body for a valid NINO without suffix") {
+      Given(s"The Benefit eligibility Info API is up and running for MA")
+      When(s"A request for MA is sent")
+
+      val payloadKey = s"MA_PTC002"
+      val payload    = PayloadMapping.getOrElse(payloadKey, fail(s"$payloadKey not found"))
+      println(payload)
+
+      val response = maService.makeRequest(payload, payloadKey)
+
+      val result: MAResponse = assertMAResponse(payload, response)
+
+      println(s"The Response Status Code is : ${response.status} ${response.statusText}")
+      println(s"""The Response Body is :
+                          ${Json.prettyPrint(Json.toJson(result))}""")
+    }
+
+    Scenario(s"MA_PTC003_502: Verify MA partial failure response when some services return errors") {
+      Given(s"The Benefit eligibility Info API is up and running for MA")
+      When(s"A request for MA is sent and Class2MAReceipts and LiabilitySummary return errors")
+
+      val payloadKey = s"MA_PTC003"
+      val payload    = PayloadMapping.getOrElse(payloadKey, fail(s"$payloadKey not found"))
+      println(payload)
+
+      val response = maService.makeRequest(payload, payloadKey)
+
+      Then("A 502 status should be returned indicating partial failure")
+      response.status shouldBe 502
+
+      println(s"The Response Status Code is : ${response.status} ${response.statusText}")
+      println(s"The Response Body is : ${response.body}")
+    }
+  }
+
+  private def assertMAResponse(payload: MARequest, response: StandaloneWSRequest#Response) = {
+    response.status shouldBe 200
+
+    val result: MAResponse = Json.parse(response.body).as[MAResponse]
+
+    Then("All major response sections should contain valid data")
+
+    // --------------------------------------------------
+    // Basic response checks
+    // --------------------------------------------------
+    result.benefitType shouldBe payload.benefitType
+    result.nationalInsuranceNumber shouldBe payload.nationalInsuranceNumber
+
+    // --------------------------------------------------
+    // Class 2 MA Receipts Result
+    // --------------------------------------------------
+    result.class2MAReceiptsResult should not be null
+    result.class2MAReceiptsResult.receiptDates should not be empty
+
+    // --------------------------------------------------
+    // Liability Summary Details Result
+    // --------------------------------------------------
+    result.liabilitySummaryDetailsResult should not be null
+    result.liabilitySummaryDetailsResult should not be empty
+
+    // Get the first item from the list and check its liability details
+    val firstLiabilityResult = result.liabilitySummaryDetailsResult.head
+    firstLiabilityResult.liabilityDetails should not be empty
+
+    // --------------------------------------------------
+    // NI Contributions & Credits
+    // --------------------------------------------------
+    result.niContributionsAndCreditsResult should not be null
+
+    // Class 1 contributions (optional for MA)
+    result.niContributionsAndCreditsResult.class1ContributionAndCredits match {
+      case Some(list) =>
+        list should not be empty
+        list.exists(_.contributionCategory.contains("STANDARD RATE")) shouldBe true
+      case None => succeed // optional for MA
+    }
+
+    // Class 2 contributions (important for MA)
+    result.niContributionsAndCreditsResult.class2ContributionAndCredits match {
+      case Some(list) =>
+        list should not be empty
+        list.exists(_.contributionCreditType == "2B") shouldBe true
+      case None => succeed // optional but expected for MA
+    }
+
+    result
+  }
+
+}
