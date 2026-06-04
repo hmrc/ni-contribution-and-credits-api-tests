240e0730e1ab9e65c158a245a869ba922a20ab73
Vedha <247391969+vedhashree-hub@users.noreply.github.com>
2026-06-04 14:42:34 +0100

    Nojira removed originator ids from helper methods (#73)

--- src/test/scala/uk/gov/hmrc/api/service/SEARCHLIGHTService.scala
+++ src/test/scala/uk/gov/hmrc/api/service/SEARCHLIGHTService.scala
@@ -41,8 +41,7 @@ class SEARCHLIGHTService extends HttpClient with MakesHttpRequestWithToken {
         Json.stringify(requestPayload),
         ("Authorization", token),
         ("Content-Type", "application/json"),
-        ("CorrelationID", correlationId),
-        "gov-uk-originator-id" -> "originatorIdBspSearchlight"
+        ("CorrelationID", correlationId)
       ),
       timeoutDuration.seconds
     )
